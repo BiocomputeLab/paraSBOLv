80d0171f5b1c859e45e8d49ac58433c86f0fd4e3
Thomas Gorochowski <tom@chofski.co.uk>
2020-04-22 18:32:03 +0100

    Update glyph renderer

--- python/00_show_glyph.py
+++ python/00_show_glyph.py
@@ -14,7 +14,12 @@ renderer = psv.GlyphRenderer(glyph_path='../glyphs_2.0/')
 
 fig = plt.figure(figsize=(6,6))
 ax = fig.add_axes([0.0, 0.0, 1.0, 1.0], frameon=False, aspect=1)
+
 renderer.draw_glyph(ax, 'CDS', (50, 50))
+renderer.draw_glyph(ax, 'Promoter', (100, 100))
+renderer.draw_glyph(ax, 'RibosomeEntrySite', (50, 150))
+renderer.draw_glyph(ax, 'Terminator', (50, 200))
+
 ax.plot([0,250], [50,50])
 ax.set_ylim([0,250])
 ax.set_xlim([0,250])
