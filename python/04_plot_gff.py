71073f59c02b2516b9b071557a0bf98dbd9f8fed
Charlie Clark <75088925+cclark1e@users.noreply.github.com>
2020-12-15 13:30:34 +0000

    update to handle interaction parameters

--- python/04_plot_gff.py
+++ python/04_plot_gff.py
@@ -44,6 +44,8 @@ def load_part_list_from_gff (filename, chrom, type_map=gffsvgtype_map, region=No
                         part_attribs['user_parameters'] = literal_eval(key_value[1])
                     elif key_value[0] == 'style_parameters':
                         part_attribs['style_parameters'] = literal_eval(key_value[1])
+                    elif key_value[0] == 'interaction':
+                        part_attribs['interaction'] = literal_eval(key_value[1])
             if part_name != None and cur_chrom == chrom and part_type in type_map.keys():
                 gff.append([part_name, type_map[part_type], part_dir, start_bp, end_bp, part_attribs])
     # Convert to part list for parasbolv
@@ -51,20 +53,31 @@ def load_part_list_from_gff (filename, chrom, type_map=gffsvgtype_map, region=No
     for gff_el in sorted(gff, key=itemgetter(3)):
         # Check for available elements and append to part list
         if not gff_el[5]:
-            part_list.append([gff_el[1], None, None])
+            part_list.append([gff_el[1], None, None, None])
         else:
-            if len(gff_el[5]) == 2:
-                part_list.append([gff_el[1], gff_el[5]['user_parameters'], gff_el[5]['style_parameters']])
+            parameters = gff_el[5].copy()
+            if len(parameters) == 3:
+                part_list.append([gff_el[1], parameters['user_parameters'], parameters['style_parameters'], parameters['interaction']])
             else:
-                if 'user_parameters' in gff_el[5]:
-                    part_list.append([gff_el[1], gff_el[5]['user_parameters'], None])
-                else:
-                    part_list.append([gff_el[1], None, gff_el[5]['style_parameters']])
-
+                if 'user_parameters' in parameters:
+                    if len(parameters) == 1:
+                        part_list.append([gff_el[1], parameters['user_parameters'], None, None])
+                    elif 'style_parameters' in parameters:
+                        part_list.append([gff_el[1], parameters['user_parameters'], parameters['style_parameters'], None])
+                    elif 'interaction' in parameters:
+                        part_list.append([gff_el[1], parameters['user_parameters'], None, parameters['interaction']])
+                elif 'style_parameters' in parameters:
+                    if len(parameters) == 1:
+                        part_list.append([gff_el[1], None, parameters['style_parameters'], None])
+                    else:
+                        part_list.append([gff_el[1], None, parameters['style_parameters'], parameters['interaction']])
+                else:   
+                    part_list.append([gff_el[1], None, None, parameters['interaction']])
     return part_list
 
 part_list = load_part_list_from_gff('./04_plot_gff.gff', 'chrom1', type_map=gffsvgtype_map)
 fig, ax, baseline_start, baseline_end = psv.render_part_list(part_list,glyph_path='../glyphs/', padding=0.3)
 ax.plot([baseline_start[0], baseline_end[0]], [baseline_start[1], baseline_end[1]], color=(0,0,0), linewidth=1.5, zorder=0)
 fig.savefig('04_plot_gff.pdf', transparent=True, dpi=300)
+
 plt.show()
