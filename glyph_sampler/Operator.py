6279975376a62095bdf81fa6bd83b7af8179ead8
Charlie Clark <charlie@cjclark.uk>
2021-05-06 18:52:56 +0100

    axis limits for size comparison

--- glyph_sampler/Operator.py
+++ glyph_sampler/Operator.py
@@ -11,7 +11,10 @@ construct = psv.Construct(part_list, renderer)
 
 fig, ax, baseline_start, baseline_end, bounds = construct.draw()
 ax.plot([baseline_start[0]-1, baseline_end[0]+1], [baseline_start[1], baseline_end[1]], color=(0,0,0), linewidth=1.5, zorder=0)
-              
+
+ax.set_ylim([-40,40])
+ax.set_xlim([-10,80])
+       
 fig.savefig('Operator.pdf', transparent=True, dpi=300)
 
 plt.show()
