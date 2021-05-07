637c829c565f2045c8a73afc16702e470c1d6838
Charlie Clark <charlie@cjclark.uk>
2021-05-07 09:07:29 +0100

    more parts for sampler

--- glyph_sampler/Unspecified.py
+++ glyph_sampler/Unspecified.py
@@ -11,7 +11,7 @@ construct = psv.Construct(part_list, renderer)
 
 fig, ax, baseline_start, baseline_end, bounds = construct.draw()
 ax.plot([baseline_start[0]-1, baseline_end[0]+1], [baseline_start[1], baseline_end[1]], color=(0,0,0), linewidth=1.5, zorder=0)
-
+              
 fig.savefig('Unspecified.pdf', transparent=True, dpi=300)
 
 plt.show()
