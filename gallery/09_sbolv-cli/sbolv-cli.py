27b3d19deaeeee35becc9599927c846783361159
Thomas Gorochowski <tom@chofski.co.uk>
2021-06-23 13:21:36 +0100

    Tidy up of the examples.

--- gallery/09_sbolv-cli/sbolv-cli.py
+++ gallery/09_sbolv-cli/sbolv-cli.py
@@ -2,37 +2,25 @@
 A tool to quickly visualse genetic
 constructs using shorthand notation.
 
-Example strings:
-
-p 1, p 2,c 3, c 5, t 6
-
-p 3,p 10,c 3,p 10,r 3,c 10,c 3,t 10,<t 3,<c 10,<r 3,<p 10
-
-Example interactions:
-
--i 0,1,in,5
-
--i 1,3,pr,10//6,7,in,4
+Example command:
+python sbolv-cli.py --string "p black,r white,c l_blue,t black,s white,p black,p blue,r white,c l_orange,t black,s white,p blue,> l_orange,<c green,<r white,< l_orange,t black" --interaction "2,6,in,blue//8,12,co,orange//8,15,co,orange" --output sbolv-cli-example.pdf
 """
 
 import parasbolv as psv
 import matplotlib.pyplot as plt
 import click
+import math
 
 
 @click.command()
 @click.option('--string', prompt='Enter your construct design', help='The shorthand string defining the construct you wish to draw.')
-@click.option('-r', '--rotation', default='', help='Rotation of the construct.')
+@click.option('-r', '--rotation', default='', help='Rotation of the construct in degrees.')
 @click.option('-g', '--gapsize', default=3.0, help='Size of the distance between the parts.')
-@click.option('-s', '--save', default=0, help='Enable or disable diagram saving.')
-@click.option('-st', '--savetype', default='jpg', help='File extension of saved diagram (enable saving with -s 1).')
-@click.option('-sp', '--savepath', default='', help='Absolute path to save diagram to (enable saving with -s 1).')
-@click.option('-f', '--fill', default=0.8, help='Value from 0-1 that determines the brightness of fills compared to the defined edge colour. \
-                                                  A value of 0 results in a white, rather than black, fill.')
+@click.option('-o', '--output', default=None, help='Filename to save output to')
 @click.option('-i', '--interaction', default='', help='interaction defined as starting "starting index,ending index,type,color". \
-                                                        Can also receive a list of such interactions seperated by a double forward slash //.')
+                                                       Can also receive a list of such interactions seperated by a double forward slash //.')
 
-def render_input(string, rotation, gapsize, interaction, save, savetype, savepath, fill):
+def render_input(string, rotation, gapsize, interaction, output=None):
     """Renders a construct from an input string.
     
     Parameters
@@ -49,9 +37,10 @@ def render_input(string, rotation, gapsize, interaction, save, savetype, savepat
         a single or multiple interactions.
     """
     parts = parse_string(string)
-    part_list = format_parts(parts, renderer, fill)
+    part_list = format_parts(parts, renderer)
     if rotation != '':
         rotation = safe_eval(rotation)
+        rotation = math.radians(rotation)
     else:
         rotation = 0.0
     if interaction != '':
@@ -61,11 +50,8 @@ def render_input(string, rotation, gapsize, interaction, save, savetype, savepat
     construct = psv.Construct(part_list, renderer, rotation=rotation, gapsize = gapsize, interaction_list = interaction)
     fig, ax, baseline_start, baseline_end, bounds = construct.draw()
     ax.plot([baseline_start[0], baseline_end[0]], [baseline_start[1], baseline_end[1]], color=(0,0,0), linewidth=1.5, zorder=0)
-    if save == 1:
-        filename = f'pSBOLv-cli-output.{savetype}'
-        if savepath != '':
-            filename = f'{savepath}/{filename}'
-        fig.savefig(filename, dpi=300)
+    if output is not None:
+        fig.savefig(output, dpi=300)
     else:
         plt.show()
 
@@ -123,23 +109,15 @@ def find_glyph(value, renderer):
     library = renderer.glyphs_library
 
     abbreviations = {
-        '?': 'Unspecified',
-        """3'""": "3' Overhang Site",
-        """5'""": "5' Overhang Site",
-        '3′': "3' Overhang Site",
-        '5′': "5' Overhang Site",
         'p': 'Promoter',
         'r': 'RibosomeEntrySite',
         'c': 'CDS',
-        'g': 'CDS', # TODO: for pigeon compatability, this should be the variant arrow (rather than pentagon) glyph
-        # f is recognised by pigeon, but not this tool
+        'g': 'CDS',
         't': 'Terminator',
-        's': 'Spacer',
+        's': 'InertDNASpacer',
         'o': 'Operator',
         '>': 'Recombination Site',
         '<': 'Recombination Site'
-
-        # I don't know what |/x/x/d are meant to represent in Pigeon
     }
 
     if len(value) == 1 and value == '<':
@@ -156,7 +134,7 @@ def find_glyph(value, renderer):
 
     return glyph, orientation
 
-  
+
 def find_color(value):
     """Finds glyph name from index.
 
@@ -169,41 +147,26 @@ def find_color(value):
     if value is tuple:
         return value
     else:
-        colordict = {'1':(51, 204, 255),
-                     '2':(0, 0, 153),
-                     '3':(0, 204, 102),
-                     '4':(0, 102, 0),
-                     '5':(255, 102, 102),
-                     '6':(255, 0, 0),
-                     '7':(255, 153, 102),
-                     '8':(255, 153, 102),
-                     '9':(255, 153, 255),
-                     '10':(204, 0, 204),
-                     '11':(255, 255, 102),
-                     '12':(255, 204, 0),
-                     '13':(140, 140, 140),
-                     '14':(0, 0, 0),
-                     'blue1':(51, 204, 255),
-                     'blue2':(0, 0, 153),
-                     'green1':(0, 204, 102),
-                     'green2':(0, 102, 0),
-                     'red1':(255, 102, 102),
-                     'red2':(255, 0, 0),
-                     'orange1':(255, 153, 102),
-                     'orange2':(255, 153, 102),
-                     'pink1':(255, 153, 255),
-                     'pink2':(204, 0, 204),
-                     'yellow1':(255, 255, 102),
-                     'yellow2':(255, 204, 0),
-                     'grey':(140, 140, 140),
-                     'gray':(140, 140, 140),
-                     'black':(0, 0, 0)}
-        value = colordict[value]
-        value = (value[0]/255, value[1]/255, value[2]/255)
-        return value
+        colordict = {'blue': ( 25/255.0, 101/255.0, 176/255.0),
+                     'l_blue': ( 82/255.0, 137/255.0, 199/255.0),
+                     'green': ( 78/255.0, 178/255.0, 101/255.0),
+                     'l_green': (144/255.0, 201/255.0, 135/255.0),
+                     'red': (220/255.0,   5/255.0,  12/255.0),
+                     'orange': (232/255.0,  96/255.0,  28/255.0),
+                     'l_orange': (241/255.0, 147/255.0,  45/255.0),
+                     'vl_orange': (246/255.0, 193/255.0,  65/255.0),
+                     'yellow': (247/255.0, 238/255.0,  85/255.0),
+                     'purple': (136/255.0,  46/255.0, 114/255.0),
+                     'l_purple': (177/255.0, 120/255.0, 166/255.0),
+                     'vl_purple': (214/255.0, 193/255.0, 222/255.0),
+                     'grey': (119/255.0, 119/255.0, 119/255.0),
+                     'vl_grey': (230/255.0, 230/255.0, 230/255.0),
+                     'black': (0, 0, 0),
+                     'white': (1, 1, 1)}
+        return colordict[value]
 
 
-def set_style_color(glyph, color, renderer, fill):
+def set_style_color(glyph, color, renderer):
     """Sets glyph path styles to specific colour.
 
     Parameters
@@ -226,17 +189,16 @@ def set_style_color(glyph, color, renderer, fill):
             color_style = {}
             for key in style.keys():
                 if 'edge' in key:
-                    color_style[key] = color
+                    if glyph in ['Promoter', 'Terminator', 'Operator']:
+                        color_style[key] = color
                 if 'facecolor' in key and 'un' not in path['class']:
-                    facecolor = (color[0]*fill, color[1]*fill, color[2]*fill)
-                    if fill == 0:
-                        facecolor = (1,1,1)
+                    facecolor = (color[0], color[1], color[2])
                     color_style[key] = facecolor
             style_dict[path['id']] = color_style
     return style_dict
 
 
-def format_parts(part_list, renderer, fill):
+def format_parts(part_list, renderer):
     """Formats parts from parsed input string
        into values usable by paraSBOLv.
 
@@ -246,12 +208,12 @@ def format_parts(part_list, renderer, fill):
         List of parts to be formatted
     renderer: obj
         ParaSBOLv glyph renderer object
-        containing glyphs librarya attribute.
+        containing glyphs library a attribute.
     """
     formatted_part_list = []
     for part in part_list:
         glyph, orientation, color, label = part[0], part[1], part[2], part[3]
-        style_dict = set_style_color(glyph, color, renderer, fill)
+        style_dict = set_style_color(glyph, color, renderer)
         if label is None:
             formatted_part = [glyph, {'orientation':orientation}, style_dict]
             formatted_part_list.append(formatted_part)
