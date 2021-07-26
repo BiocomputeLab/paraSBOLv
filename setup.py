43b81ff5a88934ea316ce26e4a30f51759b87f5a
Thomas Gorochowski <tom@chofski.co.uk>
2021-07-26 13:24:04 +0100

    Updated glyph definitions

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from distutils.core import setup
 
 setup(name='parasbolv',
 
-      version='0.1.0',
+      version='1.0.0',
 
       description='A lightweight Python library designed to simplify the rendering of ' +
                   'highly-customisable SBOL Visual glyphs and diagrams',
 
-      author='Thomas E. Gorochowski, Charlie Clark',
+      author='Thomas E. Gorochowski, Charlie Clark, James Scott-Brown',
 
-      author_email='tom@chofski.co.uk, charlieclark1.e.e.2019@bristol.ac.uk',
+      author_email='tom@chofski.co.uk, charlieclark1.e.e.2019@bristol.ac.uk, james@jamesscottbrown.com',
 
       url='https://github.com/BiocomputeLab/paraSBOLv/blob/master/parasbolv/parasbolv.py',
 
